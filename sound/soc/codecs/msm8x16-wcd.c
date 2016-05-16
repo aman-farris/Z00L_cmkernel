111631cbea9595499472748c3deb8113ffd6acfa
Abhishek Ranjan <abhishekr700@gmail.com>
2016-05-16 15:34:56 +0530

    Minor fix-Sound Control

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -549,7 +549,7 @@ int msm8x16_wcd_write(struct snd_soc_codec *codec, unsigned int reg,
 			     unsigned int value)
 {
 	int ret;
-ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 int val;
 #endif
 	struct msm8x16_wcd_priv *msm8x16_wcd = snd_soc_codec_get_drvdata(codec);
@@ -629,7 +629,8 @@ unsigned int msm8x16_wcd_read(struct snd_soc_codec *codec,
 }
 #ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 EXPORT_SYMBOL(msm8x16_wcd_read);
-static int msm8x16_wcd_codec_probe#endif
+static int msm8x16_wcd_codec_probe
+#endif
 
 static void msm8x16_wcd_boost_on(struct snd_soc_codec *codec)
 {
